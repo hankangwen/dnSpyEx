868aa886d0dfd414fc005a7adb1f40cb6301cd46
ElektroKill <37494960+ElektroKill@users.noreply.github.com>
2021-09-16 18:36:49 +0000

    Remove NRefactory cleanup from build files

--- clean-all.cmd
+++ clean-all.cmd
@@ -1,6 +1,5 @@
 @echo off
 
-pushd Extensions\ILSpy.Decompiler\NRefactory && git clean -xdf && popd || goto :error
 pushd Libraries\ICSharpCode.TreeView && git clean -xdf && popd || goto :error
 pushd Extensions\ILSpy.Decompiler\ICSharpCode.Decompiler && git clean -xdf && popd || goto :error
 pushd dnSpy\dnSpy.Images && git clean -xdf && popd || goto :error
