92743f2a34aa01ba9015057a9ffab123dce73f44
ElektroKill <elektrokilldev@protonmail.com>
2023-02-04 20:15:00 +0100

    Add missing license headers

--- Extensions/dnSpy.Debugger/dnSpy.Debugger.DotNet.CorDebug/dndbg/DotNet/CorEmbeddedResource.cs
+++ Extensions/dnSpy.Debugger/dnSpy.Debugger.DotNet.CorDebug/dndbg/DotNet/CorEmbeddedResource.cs
@@ -1,4 +1,23 @@
-﻿using dnlib.DotNet;
+﻿/*
+    Copyright (C) 2023 ElektroKill
+
+    This file is part of dnSpy
+
+    dnSpy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    dnSpy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with dnSpy.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using dnlib.DotNet;
 using dnlib.IO;
 
 namespace dndbg.DotNet {
@@ -17,7 +36,7 @@ namespace dndbg.DotNet {
 		public CorEmbeddedResource(CorModuleDef readerModule, ManifestResource mr, DataReaderFactory dataReaderFactory, uint offset, uint length)
 			: base(mr.Name, dataReaderFactory, offset, length, mr.Flags) {
 			this.readerModule = readerModule;
-			Rid = origRid =mr.Rid;
+			Rid = origRid = mr.Rid;
 			Offset = mr.Offset;
 		}
 	}
